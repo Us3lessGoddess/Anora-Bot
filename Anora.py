f8a4156435fb40d8dd7e57e6ebdafced922e55b8
Us3lessGoddess <89499124+Us3lessGoddess@users.noreply.github.com>
2026-08-20 12:19:09 +0800

    Add files via upload

--- Anora.py
+++ Anora.py
@@ -73,7 +73,8 @@ def home():
     return "Bot is alive"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
